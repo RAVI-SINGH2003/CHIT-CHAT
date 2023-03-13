0dc34593ea7a08d118ed0c47b032f3cd0f559ebe
RAVI-SINGH <ravirainbow2003@gmail.com>
2023-03-13 16:37:54 +0530

    version7

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const { socket } = require("socket.io");
+const cors = require("cors");
 
 dotenv.config();
 
 connnectDB();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
